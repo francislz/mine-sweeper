d7d4a21bab350b7d9d114ff0068c1505e566ac64
franciscone-junior <franciscone.junior@ibm.com>
2024-01-06 08:43:11 -0300

    test(uat-5.3): 3 neighbor bombs on TOP, LEFT and BOTTOM

--- src/mineSweeper.test.js
+++ src/mineSweeper.test.js
@@ -110,4 +110,15 @@ describe('calculating the number of neighbor bombs', () => {
     const board = addBombsToBoard(bombs, 3);
     expect(calculateNumberOfNeighborBombs(board, squareToClear)).toBe(2);
   });
+
+  it('should calculate 3 neighbor bombs, on: TOP, LEFT and BOTTOM', () => {
+    const squareToClear = { row: 1, column: 1 };
+    const bombs = [
+      [0, 1],
+      [1, 0],
+      [2, 1],
+    ];
+    const board = addBombsToBoard(bombs, 3);
+    expect(calculateNumberOfNeighborBombs(board, squareToClear)).toBe(3);
+  });
 });
